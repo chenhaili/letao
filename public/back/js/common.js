12fd2cdd20272f6079590344aa9000fb22dbc2b9
ChenL <luckyGirl_li@163.com>
2018-05-10 15:20:17 +0800

    进度条功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,19 @@
 /**
  * Created by chenli on 2018/5/9.
  */
+
+// 禁用进度条
+NProgress.configure({ showSpinner: false });
+
+// 开启进度条
+$(document).ajaxStart(function () {
+  NProgress.start();
+});
+
+// 模拟网络环境
+$(document).ajaxStop(function () {
+  setTimeout(function () {
+    // 结束进度条
+    NProgress.done();
+  },500);
+});
